a9aec7ab6124900d7a49cce90e90a819ef7f6026
Ramūnas <ramunasyla@gmail.com>
2017-09-18 11:44:06 +0300

    lt

--- orders.php
+++ orders.php
@@ -64,7 +64,7 @@ $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a h
 $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
 
 // Display the paging information
-echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+echo '<div id="paging"><p>', $prevlink, ' Puslapis ', $page, ' iš ', $pages, ' puslapių ', ' , įrašų iš viso ', $total, $nextlink, ' </p></div>';
 
 
 $mysqli->close();
